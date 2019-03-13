1ac3009ba28fff794d9fc18ad73d480317e34e80
Amandine Turpin <amandyne.turpin@gmail.com>
2019-03-13 22:04:57 +0100

    Gestionnaire de liens : add web API

---
+++ 02 Gestionnaire de Liens/README.md
@@ -0,0 +1,16 @@
+# Gestionnaire de Liens
+
+Création d'un gestionnaire de liens en utilisant un serveur web externe.
+
+## Critéres de validation
+
+* Les liens affichés sont récupérés depuis le serveur.
+* Le nouveau lien n’est affiché sur la page qu’en cas de succès de l’ajout sur le serveur.
+* Le formulaire d’ajout est remplacé par le bouton “Ajouter un lien” quel que soit le résultat de l’ajout sur le serveur.
+* Contrairement à l’activité 2, le rechargement de la page web affiche toujours le nouveau lien puisque celui-ci est sauvegardé sur le serveur.
+* Les communications avec le serveur utilisent les fonctions ajaxGet et ajaxPost définies dans le cours.
+* Les variables JavaScript doivent respecter la norme camelCase et le fichier liensweb.js doit être correctement indenté.
+
+![Gestionnaire de Liens](https://static.oc-static.com/prod/courses/files/creez-des-pages-web-interactives-avec-javascript/activite_3_demo.gif)
+
+Source : [OpenClassRooms - Créez des pages web interactives avec JavaScript](https://openclassrooms.com/fr/courses/3306901-creez-des-pages-web-interactives-avec-javascript)
--- 02 Gestionnaire de Liens/js/liensweb.js
+++ 02 Gestionnaire de Liens/js/liensweb.js
@@ -1,161 +1,121 @@
 /* 
-Activité 2
+Activité 3
 */
 
-// Liste des liens Web à afficher. Un lien est défini par :
-// - son titre
-// - son URL
-// - son auteur (la personne qui l'a publié)
-var listeLiens = [
-    {
-        titre: "So Foot",
-        url: "http://sofoot.com",
-        auteur: "yann.usaille"
-    },
-    {
-        titre: "Guide d'autodéfense numérique",
-        url: "http://guide.boum.org",
-        auteur: "paulochon"
-    },
-    {
-        titre: "L'encyclopédie en ligne Wikipedia",
-        url: "http://Wikipedia.org",
-        auteur: "annie.zette"
-    }
-];
+var contenuElt = document.getElementById("contenu");
 
-// Affiche tous les lients
-function showAllLink() {
+// Parcours de la liste des liens et ajout d'un élément au DOM pour chaque lien
+function showAllLinks() {
     // Suppression des noeuds enfants
     while (contenu.firstChild) {
         contenu.removeChild(contenu.firstChild);
     }
-    // Ajout du bouton 'Ajouter un lien'
-    createAddButton();
 
-    // Parcours de la liste des liens et ajout d'un élément au DOM pour chaque lien
-    listeLiens.forEach(function (lien) {
-        var elementLien = creerElementLien(lien);
-        contenu.appendChild(elementLien);
+    // Récupération des liens distants
+    ajaxGet('https://oc-jswebsrv.herokuapp.com/api/liens', function (response) {
+        let linksList = JSON.parse(response);
+        linksList.forEach(function (lien) {
+            var lienElt = creerElementLien(lien);
+            contenuElt.appendChild(lienElt);
+        });
     });
 }
 
 // Crée et renvoie un élément DOM affichant les données d'un lien
 // Le paramètre lien est un objet JS représentant un lien
 function creerElementLien(lien) {
-    var titreLien = document.createElement("a");
-    titreLien.href = lien.url;
-    titreLien.style.color = "#428bca";
-    titreLien.style.textDecoration = "none";
-    titreLien.style.marginRight = "5px";
-    titreLien.appendChild(document.createTextNode(lien.titre));
+    var titreElt = document.createElement("a");
+    titreElt.href = lien.url;
+    titreElt.style.color = "#428bca";
+    titreElt.style.textDecoration = "none";
+    titreElt.style.marginRight = "5px";
+    titreElt.appendChild(document.createTextNode(lien.titre));
 
-    var urlLien = document.createElement("span");
-    urlLien.appendChild(document.createTextNode(lien.url));
+    var urlElt = document.createElement("span");
+    urlElt.appendChild(document.createTextNode(lien.url));
 
     // Cette ligne contient le titre et l'URL du lien
-    var ligneTitre = document.createElement("h4");
-    ligneTitre.style.margin = "0px";
-    ligneTitre.appendChild(titreLien);
-    ligneTitre.appendChild(urlLien);
+    var ligneTitreElt = document.createElement("h4");
+    ligneTitreElt.style.margin = "0px";
+    ligneTitreElt.appendChild(titreElt);
+    ligneTitreElt.appendChild(urlElt);
 
     // Cette ligne contient l'auteur
-    var ligneDetails = document.createElement("span");
-    ligneDetails.appendChild(document.createTextNode("Ajouté par " + lien.auteur));
+    var ligneDetailsElt = document.createElement("span");
+    ligneDetailsElt.appendChild(document.createTextNode("Ajouté par " + lien.auteur));
 
-    var divLien = document.createElement("div");
-    divLien.classList.add("lien");
-    divLien.appendChild(ligneTitre);
-    divLien.appendChild(ligneDetails);
+    var divLienElt = document.createElement("div");
+    divLienElt.classList.add("lien");
+    divLienElt.appendChild(ligneTitreElt);
+    divLienElt.appendChild(ligneDetailsElt);
 
-    return divLien;
+    return divLienElt;
 }
 
-// Creation du bouton Ajouter un lien
-function createAddButton() {
-    var addButton = document.createElement('button');
-    addButton.textContent = 'Ajouter un lien';
-    addButton.style.marginBottom = '20px';
-    // au clic, remplacement du bouton par le formulaire
-    addButton.addEventListener('click', function () {
-        var addForm = createForm();
-        contenu.replaceChild(addForm, addButton);
+// Crée et renvoie un élément DOM de type input
+function creerElementInput(placeholder, taille) {
+    var inputElt = document.createElement("input");
+    inputElt.type = "text";
+    inputElt.setAttribute("placeholder", placeholder);
+    inputElt.setAttribute("size", taille);
+    inputElt.setAttribute("required", "true");
+    return inputElt;
+}
+
+var ajouterLienElt = document.getElementById("ajoutLien");
+// Gère l'ajout d'un nouveau lien
+ajouterLienElt.addEventListener("click", function () {
+    var auteurElt = creerElementInput("Entrez votre nom", 20);
+    var titreElt = creerElementInput("Entrez le titre du lien", 40);
+    var urlElt = creerElementInput("Entrez l'URL du lien", 40);
+
+    var ajoutElt = document.createElement("input");
+    ajoutElt.type = "submit";
+    ajoutElt.value = "Ajouter";
+
+    var formAjoutElt = document.createElement("form");
+    formAjoutElt.appendChild(auteurElt);
+    formAjoutElt.appendChild(titreElt);
+    formAjoutElt.appendChild(urlElt);
+    formAjoutElt.appendChild(ajoutElt);
+
+    var p = document.querySelector("p");
+    // Remplace le bouton d'ajout par le formulaire d'ajout
+    p.replaceChild(formAjoutElt, ajouterLienElt);
+
+    // Ajoute le nouveau lien
+    formAjoutElt.addEventListener("submit", function (e) {
+        e.preventDefault(); // Annule la publication du formulaire
+
+        var url = urlElt.value;
+        // Si l'URL ne commence ni par "http://" ni par "https://"
+        if ((url.indexOf("http://") !== 0) && (url.indexOf("https://") !== 0)) {
+            // On la préfixe par "http://"
+            url = "http://" + url;
+        }
+
+        // Création de l'objet contenant les données du nouveau lien
+        let formData = new FormData();
+        formData.append('titre', titreElt.value);
+        formData.append('url', url);
+        formData.append('auteur', auteurElt.value);
+
+        // Envoi des données à l'API
+        ajaxPost('https://oc-jswebsrv.herokuapp.com/api/lien', formData, showAllLinks);
+
+        // Remplace le formulaire d'ajout par le bouton d'ajout
+        p.replaceChild(ajouterLienElt, formAjoutElt);
+
+        // Création du message d'information
+        var infoElt = document.createElement("div");
+        infoElt.classList.add("info");
+        infoElt.textContent = "Le lien \"" + titreElt.value + "\" a bien été ajouté.";
+        p.insertBefore(infoElt, ajouterLienElt);
+        // Suppresion du message après 2 secondes
+        setTimeout(function () {
+            p.removeChild(infoElt);
+        }, 2000);
     });
-    contenu.appendChild(addButton);
-}
+});
 
-// Creation du formulaire
-function createForm() {
-    let form = document.createElement('form');
-
-    let fieldset = document.createElement('fieldset');
-    fieldset.style.marginBottom = '20px';
-
-    // Création des différents champs
-    let fields = [
-        ['text', 'author', 'Entrez votre nom'],
-        ['text', 'title', 'Entrez le titre du lien'],
-        ['text', 'link', 'Entrez l\'URL du lien'],
-    ];
-
-    for (let field of fields) {
-        let input = document.createElement('input');
-        input.style.marginRight = '10px';
-        input.type = field[0];
-        input.required = true;
-        input.name = field[1];
-        input.placeholder = field[2];
-
-        fieldset.appendChild(input);
-    }
-
-    // Ajout du bouton submit
-    let submit = document.createElement('input');
-    submit.type = 'submit';
-    submit.textContent = 'Enregistrer';
-    fieldset.appendChild(submit);
-
-    form.appendChild(fieldset);
-    // soumission formulaire
-    form.addEventListener('submit', function (e) {
-        saveData(this);
-        e.preventDefault();
-    });
-
-    return form;
-}
-
-function saveData(form) {
-    // traitement de l'url
-    let url = form.elements.link.value;
-    if (!url.startsWith('http://') && !url.startsWith('https://')) {
-        url = 'http://' + url;
-    }
-
-    let newLink = {
-        titre: form.elements.title.value,
-        url: url,
-        auteur: form.elements.author.value
-    }
-    // Ajout du nouveau lien
-    listeLiens.unshift(newLink);
-
-    // Message de confirmation
-    let result = document.createElement('div');
-    result.style.color = '#0B3B0B';
-    result.style.padding = '5px';
-    result.style.marginBottom = '20px';
-    result.style.backgroundColor = '#58FA82';
-    result.textContent = `Le lien "${form.elements.title.value}" a bien été ajouté.`;
-    contenu.insertAdjacentElement("beforebegin", result);
-
-    setTimeout(function () {
-        document.body.removeChild(result);
-    }, 2000);
-
-    showAllLink();
-}
-
-var contenu = document.getElementById("contenu");
-
-showAllLink();
+showAllLinks();
